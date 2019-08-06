343d51a413e2eda0d3df7bb7e05ef235a38ba60c
timi95 <timi.ogunkeye@gmail.com>
2019-08-06 22:32:35 +0100

    refactoring/rethinking turtle class

---
+++ re-Turtle.ts
@@ -0,0 +1,8 @@
+class Turtle{
+
+  // when a turtle is created;
+  // x and y coordinates are initialised
+  // the angle/direction in which the turtle is pointing is setTimeout
+  // the color of the turtle is set
+  constructor(){}
+}
